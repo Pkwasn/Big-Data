8427be976a2c56047763220b44f6ea846a552aa4
Pkwasn <57606090+Pkwasn@users.noreply.github.com>
2021-10-05 11:54:38 -0400

    Update wpbc-reducer.py

--- wpbc/wpbc-reducer.py
+++ wpbc/wpbc-reducer.py
@@ -4,7 +4,7 @@
 Author: Patrick Kwasniak
 Date: 10/2/2021
 Purpose: Reducer for Wisconsin Prognostic Breast Cancer (WPBC)
-   
+
 	1. What is the average radius of malignant tumor cells
 	2. How many cases are there where the benign tumor cells have more radius
 	than the average clump thickness/tumor radius of malignant tumor cells
@@ -14,29 +14,34 @@ import sys
 
 lines = sys.stdin.readlines()
 average_tumor = {}
+count = {}
 last_key = None
 
 for line in lines:
 	line = line.strip()
 	key, radius = line.split('\t')
 	radius = float(radius)
-	
+
 	if last_key == key:
-		average_tumor[key] = (average_tumor[key] + radius)/2
-	elif last_key == None:
-		average_tumor[key] = radius
-		last_key = key
+            average_tumor[key] = average_tumor[key] + radius
+            count[key] += 1
+	else:
+            average_tumor[key] = radius
+            count[key] = 1
+            last_key = key
 
-print('%s\t%.2f %s' % ('\nAverage Tumor Size:', average_tumor[last_key],'units'))
+for key in average_tumor.keys():
+    print('%s: %.2f %s' % (key + " Average Tumor Size",average_tumor[key]/count[key],'cm'))
 
-tumor_count = 0 									# Count for how many cases
-											# have a greater radius than the 
-for line in lines:									# average clump thickness
-	line = line.strip()
-	key, radius = line.split('\t')
-	radius = float(radius)	
-	
-	if radius > average_tumor[last_key]:
-		tumor_count += 1
+large_benign_count = {'B':0}
 
-print('%s\t%s %s' % ('Number of tumors with radius larger than average:',tumor_count,'Cases\n'))
+for line in lines:
+    line = line.strip()
+    key, radius = line.split('\t')
+    radius = float(radius)
+
+    if key == "B" and radius > average_tumor['M']/count['M']:
+        large_benign_count['B'] += 1
+
+
+print('%s %s cases' % ('Number of Cases w/ Benign > Avg Malignant:',large_benign_count['B']))
